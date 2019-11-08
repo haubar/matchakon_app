cc79722aa3aa31761cc37348b778d51665e391f2
haubar <haubarclack@gmail.com>
2019-11-08 19:11:28 +0800

    update head setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+const env = require('dotenv').config()
+
 
 export default {
   mode: 'universal',
@@ -5,14 +7,38 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || 'matchakon 抹茶',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      {
+        property: 'og:title',
+        content: '抹茶控',
+        template: chunk => `${chunk} - 抹茶控`,
+        vmid: 'og:title'
+      },
+      {
+        property: 'og:locale',
+        content: 'zh_TW',
+        vmid: 'og:locale'
+      },
+      {
+        property: 'og:site_name',
+        content: 'matchakon 抹茶控',
+        vmid: 'og:site_name'
+      },
+      {
+        property: 'og:url',
+        content: 'matchakon.cc',
+        vmid: 'og:url'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      // { src: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js', async: true, defer: true }
     ]
   },
   /*
@@ -23,7 +49,11 @@ export default {
   ** Global CSS
   */
   css: [
-    'element-ui/lib/theme-chalk/index.css'
+    'element-ui/lib/theme-chalk/index.css',
+    '@/assets/vendor/bootstrap/css/bootstrap.min.css',
+	  '@/assets/css/grayscale.css',
+	  '@/assets/css/infinite-scroll-docs.css',
+	  '@/assets/css-extend/extend-grayscale.css'
   ],
   /*
   ** Plugins to load before mounting the App

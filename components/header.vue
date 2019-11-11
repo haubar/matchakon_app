41e6294d0d4dd29c6ad1fd640c562116dd19ee21
haubar <haubarclack@gmail.com>
2019-11-11 18:28:53 +0800

    update template for default index

--- components/header.vue
+++ components/header.vue
@@ -2,16 +2,16 @@
   nav#mainNav.navbar.navbar-expand-lg.navbar-light.fixed-top.shadow
       .container
           a.navbar-brand.js-scroll-trigger(href='#page-top')
-            img.img-fluid(src='img/matchakon_logo.png', alt='matchakon-logo')
+            img.img-fluid(src='/img/matchakon_logo.png', alt='matchakon-logo')
           button.navbar-toggler.navbar-toggler-right(type='button', data-toggle='collapse', data-target='#navbarResponsive', aria-controls='navbarResponsive', aria-expanded='false', aria-label='Toggle navigation')
             | Menu 
             i.fas.fa-bars
           #navbarResponsive.collapse.navbar-collapse
             ul.navbar-nav.ml-auto.matchakon_topnav
               li.nav-item
-                a.nav-link.js-scroll-trigger(href='/index-map.html') 地圖
+                a.nav-link.js-scroll-trigger(href='/map') 地圖
               li.nav-item
-                a.nav-link.js-scroll-trigger(href='/index-share.html') 分享
+                a.nav-link.js-scroll-trigger(href='/share') 分享
               li.nav-item
-                a.nav-link.js-scroll-trigger(href='/index-contact.html') 聯絡我們
+                a.nav-link.js-scroll-trigger(href='/contact') 聯絡我們
 </template>

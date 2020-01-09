ed04a3dac16ac7f5e000dd9f733760afdb2922b5
haubar <haubarclack@gmail.com>
2020-01-10 01:46:53 +0800

    update embed & path

--- pages/share.vue
+++ pages/share.vue
@@ -1,16 +1,21 @@
 <template lang="pug">
  .masthead.h-auto.mh-auto
   .mx-auto.text-center.d-none.d-sm-block
-    h1.mx-auto.my-0 matchakon{{id}}
+    h1.mx-auto.my-0 matchakon
     h2.mx-auto.text-black-50.mb-4.texr-share 分享 / SHARE
   section#about.about-section.text-center.bg-location
     .container.container--wide
       form.form-inline.d-flex
         input.form-control.flex-fill.mb-3(placeholder='Search')
-      .image-grid.are-images-unloaded(data-js='image-grid', v-for="val in data")
+      .image-grid.are-images-unloaded(data-js='image-grid', v-for="article in data")
         .image-grid__col-sizer
         .image-grid__gutter-sizer
-        img.image-grid__image.rounded(:src=`val.thumbnail_src`, alt='')
+          instagram-embed(
+            :url="'https://www.instagram.com/p/'+article.shortcode+'/'"
+            :max-width='150'
+            :hide-caption="true"
+            :class-name="'instagram-embed'"
+          )
 
       .scroller-status
         .loader-ellips.infinite-scroll-request
@@ -26,21 +31,22 @@
 <script>
 // import axios from '~/plugins/axios'
 import axios from 'axios'
+import InstagramEmbed from 'vue-instagram-embed';
 
 export default {
   data () {
     return { }
   },
-  
+  components: {
+    InstagramEmbed
+  },
   async asyncData({ params }) {
     const {data} = await axios.get('https://us-central1-matchakon-api.cloudfunctions.net/matcha')
-    // if (params.code){
-    //    const {data} = await axios.get('https://us-central1-matchakon-api.cloudfunctions.net/matcha/'+params.code)
-    // }
-      await console.log(data) 
+    if (params.code){
+       const {data} = await axios.get('https://us-central1-matchakon-api.cloudfunctions.net/matcha/'+params.code)
+    }
       return { 
         data:data,
-        id:data[0].id
       }
       // await console.log(id)
       // await console.log(999)

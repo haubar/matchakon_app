8923c821382dd77d95563007958f47fa376b2317
haubar <haubarclack@gmail.com>
2020-02-11 20:24:27 +0800

    修正地圖顯示dom

--- pages/map.vue
+++ pages/map.vue
@@ -1,13 +1,11 @@
 <template lang="pug">
   header.masthead
     .mx-auto.text-center.d-none.d-sm-block
-      h1.mx-auto.my-0 matchakon
-      h2.mx-auto.text-black-50.mt-2.mb-5 抹茶甜點的相關分布
-    #map-wrap.container.d-flex.h-100.align-items-center
+    #map-wrap(style={height:'100vh'})
       no-ssr
         l-map(:zoom=13 :center="[25.0418254,121.5149875]")
           l-tile-layer(url="http://{s}.tile.osm.org/{z}/{x}/{y}.png")
-          l-marker(:lat-lng="[47.413220, -1.219482]")
+          l-marker(:lat-lng="[25.0418254,121.5149875]")
 </template>
 <script>
 export default {
